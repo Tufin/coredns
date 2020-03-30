be7bd6fab404b8fdbbdf3d8e5d72a255b650c04f
uriaflalo <uriaflalo@gmail.com>
2020-03-30 15:13:35 +0300

    remove test github.com/coredns/coredns/plugin/whitelist

--- core/plugin/zplugin.go
+++ core/plugin/zplugin.go
@@ -36,7 +36,6 @@ import (
 	_ "github.com/coredns/coredns/plugin/template"
 	_ "github.com/coredns/coredns/plugin/tls"
 	_ "github.com/coredns/coredns/plugin/trace"
-	_ "github.com/coredns/coredns/plugin/whitelist"
 	_ "github.com/coredns/coredns/plugin/whoami"
 	_ "github.com/mholt/caddy/onevent"
 )
